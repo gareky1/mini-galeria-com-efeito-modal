265a26e51eb8f5599f3f6ba558ba0380a858969a
Henrique Lira Da Silva <henrique.2019210942@unicap.br>
2023-02-08 21:30:10 -0300

    galeria simples

---
+++ galeria.js
@@ -0,0 +1,17 @@
+let imgagens = document.querySelectorAll(".small_img");
+let modal = document.querySelector(".modal");
+let modalImg = document.querySelector("#modal_img");
+let btClose = document.querySelector("#bt_close");
+let srcVal = "";
+
+for (let i = 0; i < imgagens.length; i++) {
+  imgagens[i].addEventListener("click", function () {
+    srcVal = imgagens[i].getAttribute('src');
+    modalImg.setAttribute('src',srcVal);
+    modal.classList.toggle('modal_active')
+  });
+}
+
+btClose.addEventListener('click',function(){
+    modal.classList.toggle('modal_active')
+})
